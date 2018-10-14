a840e33768e4a9fbca40fbf18c62766baf76b282
mysterypaint <mysterypainto@gmail.com>
2018-10-13 18:19:13 -0700

    Implemented Base GUI

--- ALTTPSRAMEditor/Form1.Designer.cs
+++ ALTTPSRAMEditor/Form1.Designer.cs
@@ -28,13 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opensrmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveCTRLSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opensrmToolStripMenuItem,
+            this.saveCTRLSToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // opensrmToolStripMenuItem
+            // 
+            this.opensrmToolStripMenuItem.Name = "opensrmToolStripMenuItem";
+            this.opensrmToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.opensrmToolStripMenuItem.Text = "Open (Ctrl+O)";
+            this.opensrmToolStripMenuItem.Click += new System.EventHandler(this.opensrmToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // saveCTRLSToolStripMenuItem
+            // 
+            this.saveCTRLSToolStripMenuItem.Name = "saveCTRLSToolStripMenuItem";
+            this.saveCTRLSToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveCTRLSToolStripMenuItem.Text = "Save (Ctrl+S)";
+            this.saveCTRLSToolStripMenuItem.Click += new System.EventHandler(this.saveCTRLSToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.menuStrip1);
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "A Link to the Past SRAM Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opensrmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveCTRLSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- ALTTPSRAMEditor/Form1.cs
+++ ALTTPSRAMEditor/Form1.cs
@@ -16,5 +16,73 @@ namespace ALTTPSRAMEditor
         {
             InitializeComponent();
         }
+
+        private void opensrmToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenSRM();
+        }
+
+        private void OpenSRM()
+        {
+            OpenFileDialog fd1 = new OpenFileDialog();
+            fd1.Filter = "SRAM|*.srm"; // Filter to show .srm files only.
+            String fname = "";
+
+            if (fd1.ShowDialog().Equals(DialogResult.OK))
+            { // Prompt the user to open a file, then check if a valid file was opened.
+                fname = fd1.FileName;
+                MessageBox.Show(fname);
+            }
+        }
+
+        private void SaveSRM()
+        {
+            MessageBox.Show("Adam");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            // Terminate the program if we select "Exit" in the Menu Bar
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void saveCTRLSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveSRM();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control) // Handle CTRL shortcuts
+            {
+                switch(e.KeyCode.ToString())
+                {
+                    case "O": // CTRL+O: Open file
+                        OpenSRM();
+                        break;
+                    case "S": // CTRL+S: Save file
+                        SaveSRM();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String tool_credits = "ALTTP SRAM Editor\n- Created by mysterypaint 2018\n\nSpecial thanks to alttp.run for the reverse-engineering documentation. http://alttp.run/hacking/index.php?title=SRAM_Map";
+            MessageBox.Show(tool_credits);
+        }
     }
 }
